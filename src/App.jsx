99dad7cfac1df26d8f65c8aa6b17db51cbb59b50
DIPU DAS <ddipu0118@gmail.com>
2026-02-17 04:37:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+  return (
+    <>
+     <Navbar></Navbar>
+     <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
